e04919cd835257a89247f1ced44ab05ee6b6521f
Nikita Kondratyev <nickondr.production@gmail.com>
2020-06-08 20:40:47 +0300

    My first commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 function helloWorld() {
   console.log('Welcome to EPAM Summer2020 LAB!');
+  console.log('Welcome to EPAM Summer2020 LAB!');
+  console.log('Welcome to EPAM Summer2020 LAB!');
 }
 
 helloWorld();
