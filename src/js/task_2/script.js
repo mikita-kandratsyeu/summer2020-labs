fc2d508d0d145866fefc73dfbe2c9dd274b3836e
Mikita Kandratsyeu <nickondr.production@gmail.com>
2020-07-14 00:26:05 +0300

    Initial commit

---
+++ src/js/task_2/script.js
@@ -0,0 +1,69 @@
+// Strings
+// You shouldn't use internal functions e.g. String.prototype.startsWith()
+function reverseString(str) {
+  // your logic here
+  // can be several variants
+}
+
+function endsWith(str, substr) {
+  // your logic here
+  // can be several variants
+}
+
+function startsWith(str, substr) {
+  // your logic here
+  // can be several variants
+}
+
+function isCamelCase(str) {
+  // your logic here
+  // (‘email’) => false
+  // (‘eMail’) => false
+  // (‘e_mail’) => false
+}
+
+function isSnakeCase(str) {
+  // your logic here
+}
+
+function isKebabCase(str) {
+  // your logic here
+}
+
+function isNaN(value) {
+  // your logic here
+}
+
+function isFinite(value) {
+  // your logic here
+}
+
+function isFalsy(value) {
+  // your logic here
+}
+
+
+// Write a function that finds all the occurrences of word in a text
+// The search can be case sensitive or case insensitive
+// Use optional parameters
+
+// Functions
+
+// Given two numbers A and B, where (A <B).
+// Write a function that displays all integer numbers in interval from A to B (including borders).
+// Write a function that displays all odd integer values located
+// in the numerical interval from A to B.
+// Write a function that returns the last digit of given integer
+// as an English word.
+// Examples: 512 -> "two",
+// 1024 -> "four", 12309 -> "nine"
+
+// Closures and Scopes
+
+// Closures and Scopes. Сreate a function, which returns an array of functions,
+// which return their index
+// in the array.
+// For better understanding, here is an example:
+// var callbacks = createFunctions(5); // create an array, containing 5 functions
+// callbacks[0](); // must return 0
+// callbacks[3](); // must return 3
