90fedd5bb8b9fd41166fbfe899acd242e3c58f7b
Mikita Kandratsyeu <nickondr.production@gmail.com>
2020-07-21 21:31:50 +0300

    Initial commit

---
+++ src/js/task_3/script.js
@@ -0,0 +1,77 @@
+/**
+ * @description - should check if parameter is object, if not
+ * should return false
+ * @param obj - can be any data types
+ * @return boolean - true or false
+ */
+function isObject(obj) {
+  // your logic here
+}
+
+/**
+ * @description - should check if parameter is function, if not
+ * should return false
+ * @param obj - can be any data types
+ * @return boolean - true or false
+ */
+function isFunction(obj) {
+  // your logic here
+}
+
+/**
+ * @description Custom implementation of the function Object.keys
+ * but only for own properties
+ * @param obj can be any data types
+ * @return array of keys or error
+ */
+function keys(obj) {
+  // your logic here
+}
+
+/**
+ * @description Custom implementation of the function Object.keys
+ * for all properties
+ * @param obj can be any data types
+ * @return array of keys or error
+ */
+function allKeys(obj) {
+  // your logic here
+}
+
+/**
+ * @description Custom implementation of the function Object.values
+ * @param obj can be any data types
+ * @return array of values or error
+ */
+function values(obj) {
+  // your logic here
+}
+
+/**
+ * @description Custom implementation of the function Object.entries
+ * @param obj can be any data types
+ * @return array of pairs [key, value] or error
+ */
+function pairs(obj) {
+  // your logic here
+}
+
+/**
+ * @description Should find all properties in an object
+ * value of which are functions and collect their names into array
+ * @param obj can be any data types
+ * @return sorted in alphabetic order array of function names or error
+ */
+function functions(obj) {
+  // your logic here
+}
+
+/**
+ * @description Custom implementation of the function Function.prototype.bind
+ * @param func
+ * @param context
+ * @param arg1, arg2, argN - comma separated parameters (no ES6, e.g. ...rest)
+ */
+function bind(func, context, arg1, arg2, argN) {
+  // your logic here
+}
